fa700fdd34bbc088c237f055fdac297a3202141c
hanbyeol00 <gksquf6670@gmail.com>
2022-12-27 17:41:33 +0900

    Android

--- Hello/app/src/main/java/com/hanbyeol/hello2/FirstFragment.kt
+++ Hello/app/src/main/java/com/hanbyeol/hello2/FirstFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.hanbyeol.hello2.R
 import com.hanbyeol.hello2.databinding.FragmentFirstBinding
 
 /**
