6819937bc41f9c33c822857e7e02ed6048e4cf64
hanbyeol00 <gksquf6670@gmail.com>
2023-01-04 13:51:08 +0900

    android

---
+++ App009/app/src/main/java/com/hanbyeol/game/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.hanbyeol.game
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hanbyeol.game.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
