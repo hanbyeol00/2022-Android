6a6d0b92f27425ba983d4b2d0a660bfef0f39e07
hanbyeol00 <gksquf6670@gmail.com>
2023-01-06 11:26:43 +0900

    android

---
+++ App101/app/src/main/java/com/hanbyeol/student/data/Student.kt
@@ -0,0 +1,10 @@
+package com.hanbyeol.student.data
+
+data class Student(
+    val stNum : String? = null,
+    val stName : String? = null,
+    val stTel : String? = null,
+    val stAddr : String? = null,
+    val stDept : String? = null,
+    val stGrade : Int? = null
+)
